3b599f68e5148a9fff48cc889f6eb332c3f827dc
Macbook Air <macbookair@David-Ramirez.netgear.com>
2016-08-26 00:32:56 -0700

    add a controller and inside the controller make a property of the variable gem

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 (function(){
+	var app = angular.module('store', []);
 
+	app.controller('StoreController', function(){
+		this.product = gem;
+	})
+	var gem = {
+		name: "rock",
+		price: 3.09,
+		description: "hard and round",
+	}
 })();
